fb4ae478eb44363f2669cae4248828bed8ec7618
EvanMcRae <evanmmcrae@gmail.com>
2022-09-15 18:15:58 -0400

    Sprite flipping

--- Sword Dog Game/Assets/Scripts/PlayerMovement.cs
+++ Sword Dog Game/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator anim;
+    bool facingRight;
     [SerializeField] private float speed = 2f;
 
     void Start()
     {
+        facingRight = true;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -17,10 +19,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
+        if ((moveX < 0 && facingRight) || (moveX > 0 && !facingRight))
+        {
+            Flip();
+        }
+
         float targetVelocity = Mathf.Lerp(rb.velocity.x, moveX * speed, 0.1f);
         rb.velocity = new Vector3(targetVelocity, rb.velocity.y, 0);
 
-        anim.SetFloat("speed", targetVelocity / speed);
-        anim.SetFloat("absSpeed", Mathf.Abs(targetVelocity / speed));
+        anim.SetFloat("speed", Mathf.Abs(targetVelocity / speed));
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-1 * transform.localScale.x, 1, 1);
     }
 }
