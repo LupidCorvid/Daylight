9015a41cd7e373a6937043be66bf4d14d0f563f8
RJWMD <113476572+RJWMD@users.noreply.github.com>
2022-09-16 16:19:43 -0400

    Added text mesh pro and started dialog npcs

---
+++ Sword Dog Game/Assets/UI/DialogAssets/DialogNPC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogNPC : MonoBehaviour
+{
+    public GameObject barkFXPrefab;
+
+    public string dialog;
+
+    /*Dialogue guide:
+     * [] is escape
+     * [b] is bark 
+     * [b]
+     * 
+     * 
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Sword Dog Game/Assets/UI/DialogAssets/SpeakParticle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakParticle : MonoBehaviour
+{
+    public Vector3 velocity;
+    public Vector3 acceleration;
+
+    public float drag;
+
+    public float lifeTime = 5;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        if(startTime + lifeTime < Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
