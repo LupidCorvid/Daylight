3766f6a275132a36b680d841a964582060ee2126
LupidCorvid <lucidcanine921@gmail.com>
2022-09-15 23:45:57 -0400

    Tree

---
+++ Sword Dog Game/Assets/Scripts/TreeSwayEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSwayEffect : MonoBehaviour
+{
+    public Sprite treeTexture;
+    Mesh original;
+    MeshFilter meshFilter;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
