e6d38e530b15881d1f210758b491f1399d2da38e
EvanMcRae <evanmmcrae@gmail.com>
2022-09-17 11:15:08 -0400

    New parallaxing script

--- Sword Dog Game/Assets/Scripts/Parallax.cs
+++ Sword Dog Game/Assets/Scripts/Parallax.cs
@@ -2,27 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Copy pasted from the other branch
+//https://www.youtube.com/watch?v=wBol2xzxCOU
 public class Parallax : MonoBehaviour
 {
-    public float distance = 0;
-    public Camera cam;
-    public Vector3 startPos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam ??= Camera.main;
-        startPos = transform.position;
-    }
+    [SerializeField] private Vector2 effectMultiplier;
+    [SerializeField] private bool infiniteHorizontal, infiniteVertical;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = Vector3.zero;
+
+        // adjust tiling size for infinite scrolling
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.size = new Vector2(spriteRenderer.size.x * (infiniteHorizontal ? 3 : 1), spriteRenderer.size.y * (infiniteVertical ? 3 : 1));
+        
+        // calculate texture unit size for infinite scrolling
+        Sprite sprite = spriteRenderer.sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit * transform.localScale.x;
+        textureUnitSizeY = texture.height / sprite.pixelsPerUnit * transform.localScale.y;
+        
     }
 
     private void FixedUpdate()
     {
-        transform.position = new Vector3(startPos.x + ((startPos.x - cam.transform.position.x) * distance), startPos.y + ((startPos.y - cam.transform.position.y) * distance), transform.position.z);
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * effectMultiplier.x, deltaMovement.y * effectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+        // repeat infinitely horizontally
+        if (infiniteHorizontal && Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {  
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
+
+        // repeat infinitely vertically
+        if (infiniteVertical && Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+        {
+            float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+            transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+        }
     }
 }
