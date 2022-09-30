79ff4b8423448b7cacc2fe52455ba2c43c7db7e4
RJWMD <113476572+RJWMD@users.noreply.github.com>
2022-09-30 12:00:44 -0400

    Fix for ledge rotation and flickering on overlapping ground objects

--- Sword Dog Game/Assets/Scripts/PlayerMovement.cs
+++ Sword Dog Game/Assets/Scripts/PlayerMovement.cs
@@ -89,7 +89,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void checkIfLanding(Collider2D collision)
     {
-        if(Mathf.Pow(2, collision.gameObject.layer) == whatIsGround)
+        if(Mathf.Pow(2, collision.gameObject.layer) == whatIsGround && !isGrounded)
         {
             lastLand = Time.time;
         }
@@ -332,28 +332,19 @@ public class PlayerMovement : MonoBehaviour
         float acrossPercent = across.distance / (Mathf.Abs(upperRightCorner.x - upperLeftCorner.x));
         float acrossPercent2 = across2.distance / (Mathf.Abs(upperRightCorner.x - upperLeftCorner.x));
 
-        
 
+        bool onLedge = false;
         //Makessure that it is not reading the slope of the underside of a slope by not taking abs val. 
         if (acrossPercent2 - acrossPercent < .01)//If issues arise get abs value
         {
             slopeSideAngle = 0;
+            onLedge = true;
             if (acrossPercent != 0 && acrossPercent2 != 0 && (!onlyRotateWhenGrounded /*|| isGrounded*/))
                 return;
         }
-        if(!float.IsNaN(unsmoothedSlope))
+        if(!float.IsNaN(unsmoothedSlope) && !onLedge)
             slopeSideAngle = unsmoothedSlope * Mathf.Lerp(1, 0, (Mathf.Abs((acrossPercent/.5f) - 1)));
 
-        if (onlyRotateWhenGrounded)
-        {
-            //if (nearHit.distance > colliderSize.y + slopeCheckDistance)
-            //    slopeSideAngle = lastGroundedSlope;
-            //else
-            //{
-            //    //Add variable for the last slope when grounded. The interpolate between that and the new grounded slope through the distance to the ground
-            //    slopeSideAngle = Mathf.Lerp(slopeSideAngle, lastGroundedSlope, (nearHit.distance - colliderSize.y)/slopeCheckDistance);
-            //}
-        }
         if (!isGrounded)
         {
             const float ROTATION_INTENSITY = 75;
