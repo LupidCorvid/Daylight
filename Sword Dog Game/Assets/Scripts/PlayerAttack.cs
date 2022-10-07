c7bc94d638388fbdc30286722be5c4203856bb4d
EvanMcRae <evanmmcrae@gmail.com>
2022-10-07 16:00:15 -0400

    Fix attack cooldown logic

--- Sword Dog Game/Assets/Scripts/PlayerAttack.cs
+++ Sword Dog Game/Assets/Scripts/PlayerAttack.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private Animator anim;
-    private int attackCombo = 0;
-    private bool isAttacking = false, canAttack = true;
+    public int attackCombo = 0;
+    public bool isAttacking = false, canAttack = true;
     [SerializeField] private float cooldownLength = 0.5f;
+    public float attackCooldown = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,34 +19,41 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && canAttack && attackCombo < 3)
+        // attack cooldown
+        if (attackCooldown > 0)
         {
-            isAttacking = true;
-            attackCombo++;
-
-            anim.SetTrigger("attack" + attackCombo);
+            canAttack = false;
+            attackCooldown -= Time.deltaTime;
+        }
+        if (attackCooldown <= 0 && !isAttacking)
+        {
+            attackCombo = 0;
+            canAttack = true;
+            attackCooldown = 0;
         }
 
+        // attack input detection + combo tracking
+        if (Input.GetMouseButtonDown(0) && canAttack && attackCombo < 3)
+        {
+            Debug.Log("attack");
+            isAttacking = true;
+            attackCombo++;
+            anim.SetTrigger("attack" + attackCombo);
+        }
+        
+        // perhaps useful in the future for preventing sprint/jump from interrupting attack
         anim.SetBool("attacking", isAttacking);
     }
 
+    // stops attacks -- called from animation events in return states
     private void StopAttack()
     {
         attackCombo = 0;
         isAttacking = false;
-        StopCoroutine("AttackCooldown");
-        StartCoroutine(AttackCooldown(cooldownLength));
-        
+        attackCooldown = cooldownLength;
         for (int i = 1; i <= 3; i++)
         {
             anim.ResetTrigger("attack" + i);
         }
-    }
-
-    IEnumerator AttackCooldown(float seconds)
-    {
-        canAttack = false;
-        yield return new WaitForSeconds(seconds);
-        canAttack = true;
     }
 }
