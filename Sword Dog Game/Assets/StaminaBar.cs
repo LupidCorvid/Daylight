d2982eb08085c07fd9f271df4ead314222f92e5d
RJWMD <113476572+RJWMD@users.noreply.github.com>
2022-10-21 16:12:43 -0400

    Attempted to start adding stamina

---
+++ Sword Dog Game/Assets/StaminaBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar : MonoBehaviour
+{
+    public float fillAmount = 1;
+    private float defaultWidth;
+    private float basePosition;
+
+    private float originalWidth;
+
+    RectTransform rect;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        defaultWidth = rect.localScale.x;
+        basePosition = rect.position.x;
+        basePosition = rect.rect.x;
+        basePosition = rect.position.x - rect.rect.width/2;
+        //originalWidth = rect.rect.width;
+    }
+    void Update()
+    {
+        setFillAmount(fillAmount);
+    }
+    public void setFillAmount(float amount)
+    {
+        rect.localScale = new Vector3(defaultWidth * amount, rect.localScale.y, rect.localScale.z);
+        rect.position = new Vector3(basePosition + (rect.rect.width /** rect.localScale.x*/)/2, rect.position.y, rect.position.z);
+        //(basePosition - (rect.localScale.x))
+    }
+}
