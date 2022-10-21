14aa32864ec2d5b20a5ecc88ade6592aa070f79b
EvanMcRae <evanmmcrae@gmail.com>
2022-10-21 17:08:22 -0400

    Fix all the things

--- Sword Dog Game/Assets/StaminaBar.cs
+++ Sword Dog Game/Assets/StaminaBar.cs
@@ -1,36 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaBar : MonoBehaviour
 {
-    public float fillAmount = 1;
-    private float defaultWidth;
-    private float basePosition;
+    [SerializeField] private Slider slider;
 
-    private float originalWidth;
-
-    RectTransform rect;
-
-    private void Awake()
-    {
-        rect = GetComponent<RectTransform>();
-        defaultWidth = rect.localScale.x;
-        basePosition = rect.position.x;
-        basePosition = rect.rect.x;
-        basePosition = rect.position.x - rect.rect.width/2;
-        Debug.Log(rect.position.x);
-        Debug.Log(basePosition);
-        //originalWidth = rect.rect.width;
-    }
     void Update()
     {
-        setFillAmount(fillAmount);
-    }
-    public void setFillAmount(float amount)
-    {
-        rect.localScale = new Vector3(defaultWidth * amount, rect.localScale.y, rect.localScale.z);
-        rect.position = new Vector3(basePosition + (rect.rect.width * rect.localScale.x) /1, rect.position.y, rect.position.z);
-        //(basePosition - (rect.localScale.x))
+        slider.value = PlayerMovement.controller.stamina / PlayerMovement.controller.maxStamina;
     }
 }
