522ddf79a7728e79b3343e4836b6059b671046fb
RJWMD <113476572+RJWMD@users.noreply.github.com>
2022-11-23 16:40:19 -0500

    Changes to vine
    Changed sprite and made the loader for it work (mostly)

--- Sword Dog Game/Assets/Scripts/Vines/VineController.cs
+++ Sword Dog Game/Assets/Scripts/Vines/VineController.cs
@@ -9,21 +9,58 @@ public class VineController : MonoBehaviour
 
     public float length = 5;
 
-    public int segmentNums = 5;
+    public int numSegments = 5;
 
-    List<GameObject> segments = new List<GameObject>();
+    Rigidbody2D rb;
 
-    GameObject segmentPrefab;
+    List<VineSegment> segments = new List<VineSegment>();
+
+    public GameObject segmentPrefab;
 
     public bool generateVinesRuntime = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (generateVinesRuntime)
+            loadSegments();
+    }
 
     void AddNewSegment()
     {
         GameObject newSegment = Instantiate(segmentPrefab, transform);
+        Collider2D sgmntCldr = newSegment.GetComponent<Collider2D>();
+        VineSegment vinePart = newSegment.GetComponent<VineSegment>();
+
+        float sizePerSegment = (length / numSegments) * sgmntCldr.bounds.size.y;
+
+
+        newSegment.transform.localScale = new Vector3((length / numSegments), (length / numSegments), 1);
+
+        
+        if (segments.Count > 0)
+        {
+            vinePart.transform.position = segments[segments.Count - 1].transform.position - (sizePerSegment  * Vector3.up);
+            vinePart.connection.connectedBody = segments[segments.Count - 1].rb;
+            //vinePart.connection.connectedAnchor = Vector2.down * 1;
+            vinePart.connection.connectedAnchor = sgmntCldr.bounds.extents.y * Vector2.down;
+        }
+        else
+        {
+            vinePart.transform.position = transform.position - (sizePerSegment / 2 * Vector3.up);
+            vinePart.connection.connectedBody = rb;
+        }
+
+        vinePart.connection.anchor = Vector2.up * sgmntCldr.bounds.extents.y;
+
+        segments.Add(vinePart);
     }
 
     public void loadSegments()
     {
-
+        for(int i = 0; i < numSegments; i++)
+        {
+            AddNewSegment();
+        }
     }
 }
