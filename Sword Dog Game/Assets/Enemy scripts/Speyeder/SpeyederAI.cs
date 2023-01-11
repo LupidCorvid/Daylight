c4ba016516598a8f6ad77556f9b844dd86914d1c
RJWMD <113476572+RJWMD@users.noreply.github.com>
2023-01-11 15:39:08 -0500

    Made spider no longer freely rotate

--- Sword Dog Game/Assets/Enemy scripts/Speyeder/SpeyederAI.cs
+++ Sword Dog Game/Assets/Enemy scripts/Speyeder/SpeyederAI.cs
@@ -54,6 +54,8 @@ public class SpeyederAI : BaseAI
     {
         base.FixedUpdate();
 
+        transform.rotation = Quaternion.Euler(0, 0, -Vector2.SignedAngle(web.connectedBody.transform.position - transform.position, Vector2.up));
+
         switch (state)
         {
             case states.idle:
