4a85de13f2692d980900a48911563cef3f2e9561
RJWMD <113476572+RJWMD@users.noreply.github.com>
2023-01-31 22:00:59 -0500

    Added reactions to leaves being moved quickly

--- Sword Dog Game/Assets/Scripts/LeavesSway.cs
+++ Sword Dog Game/Assets/Scripts/LeavesSway.cs
@@ -14,12 +14,30 @@ public class LeavesSway : MonoBehaviour
 
     public bool useVelocity = true;
 
+    public ParticleSystem particleHandler;
+
+    private Vector3 lastPosition;
+
+    public float leavesDropSensitivity = 1;
+
+    public bool waitUntilRestingToDropAgain = false;
+
+    bool rested = true;
+
     public void Start()
     {
         lastRotation = transform.root.eulerAngles.z;
+        particleHandler = GetComponentInChildren<ParticleSystem>();
+        lastPosition = transform.position;
     }
 
     public void FixedUpdate()
+    {
+        updateRotations();
+        checkSpawnParticles();
+    }
+
+    public void updateRotations()
     {
         float newRotation;
         if (useVelocity)
@@ -32,5 +50,18 @@ public class LeavesSway : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + (lastRotation - newRotation));
         lastRotation = newRotation;
+    }
+
+    public void checkSpawnParticles()
+    {
+        if((transform.position - lastPosition).magnitude > leavesDropSensitivity * Time.deltaTime)
+        {
+            particleHandler.Emit(Mathf.Clamp((int)(Random.Range(15, 30) * (transform.position - lastPosition).magnitude), 1, 30));
+            rested = false;
+        }
+        if ((transform.position - lastPosition).magnitude <= .01)
+            rested = true;
+
+        lastPosition = transform.position;
     }
 }
