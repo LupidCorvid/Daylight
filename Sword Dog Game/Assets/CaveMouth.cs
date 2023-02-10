a1be7907fa7fcc7e2e7ce58e8c1a279347138a17
EvanMcRae <evanmmcrae@gmail.com>
2023-02-10 16:52:40 -0500

    attempts to fix cave mouth sloping

---
+++ Sword Dog Game/Assets/CaveMouth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveMouth : MonoBehaviour
+{
+    [SerializeField] private Collider2D edge1, edge2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        edge1.enabled = PlayerMovement.instance.transform.position.y > -9;
+        edge2.enabled = PlayerMovement.instance.transform.position.y > -9;
+    }
+}
