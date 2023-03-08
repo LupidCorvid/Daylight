0daa043331f77932fe6c07d8875e049ea523a83b
RJWMD <113476572+RJWMD@users.noreply.github.com>
2023-03-07 22:11:07 -0500

    added a particle system version, which has significantly lower performance impact when in large quantities

--- Sword Dog Game/Assets/Enemy scripts/BugShroom/BugShroomAttack.cs
+++ Sword Dog Game/Assets/Enemy scripts/BugShroom/BugShroomAttack.cs
@@ -9,18 +9,39 @@ public class BugShroomAttack : MonoBehaviour
     public float SporeCloudRadius = 3;
     public float SporeCloudAngleRange = 360;
 
+    public bool UseParticleSystem = true;
+
+    public ParticleSystem pSys;
 
     public void Attack()
     {
-        for(int i = 0; i < numSporeClouds; i++)
+        if (!UseParticleSystem)
         {
-            GameObject cloud = Instantiate(sporeCloudPrefab, transform.position, transform.rotation, TempObjectsHolder.asTransform);
-            Rigidbody2D cloudPhys = cloud.GetComponent<Rigidbody2D>();
-            float angle = Random.Range(0, SporeCloudAngleRange);
-            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-            cloudPhys.velocity = direction * Random.Range(.25f, SporeCloudRadius) * cloudPhys.drag;
+            for (int i = 0; i < numSporeClouds; i++)
+            {
+                GameObject cloud = Instantiate(sporeCloudPrefab, transform.position, transform.rotation, TempObjectsHolder.asTransform);
+                Rigidbody2D cloudPhys = cloud.GetComponent<Rigidbody2D>();
+                float angle = Random.Range(0, SporeCloudAngleRange);
+                Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                cloudPhys.velocity = direction * Random.Range(.25f, SporeCloudRadius) * cloudPhys.drag;
 
-            //Set speed of cloud
+                //Set speed of cloud
+            }
+        }
+        else
+        {
+            for(int i = 0; i < numSporeClouds; i++)
+            {
+                float angle = Random.Range(0, SporeCloudAngleRange);
+                Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                ParticleSystem.EmitParams particleParams = new ParticleSystem.EmitParams()
+                {
+                    velocity = direction * Random.Range(.1f, SporeCloudRadius) * pSys.limitVelocityOverLifetime.drag.constant
+                    
+                };
+                pSys.Emit(particleParams, 1);
+
+            }
         }
     }
 }
