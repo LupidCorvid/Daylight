0f36f3908af27be83fb48e6a498cbda854d609bb
RJWMD <113476572+RJWMD@users.noreply.github.com>
2023-03-31 15:13:19 -0400

    Added thorns hazard. Thorns in forest 2 now do damage

---
+++ Sword Dog Game/Assets/ThornsHazard.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornsHazard : MonoBehaviour
+{
+    public int damage = 1;
+    public float damageCooldown = 1f;
+    public bool requireMovement = true;
+
+
+    float lastDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerHealth hit = collision.GetComponent<PlayerHealth>();
+        if(hit != null)
+        {
+            if(lastDamage + damageCooldown < Time.time && (!requireMovement || (collision.GetComponent<Rigidbody2D>().velocity.magnitude > .1f)))
+            {
+                hit.TakeDamage(damage);
+                lastDamage = Time.time;
+            }
+        }
+    }
+}
