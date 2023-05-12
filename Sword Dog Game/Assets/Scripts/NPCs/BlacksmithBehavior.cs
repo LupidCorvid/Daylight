54ed08c197659977e0d8fc6292433b356c62089c
LupidCorvid <lucidcanine921@gmail.com>
2023-05-12 12:39:35 -0400

    Added blacksmith assets and dialog

---
+++ Sword Dog Game/Assets/Scripts/NPCs/BlacksmithBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlacksmithBehavior : DialogNPC
+{
+    Animator anim;
+    float waitToLook = 0f;
+    bool talking, finishTalkingSequence = false;
+
+    Vector2 playerPosition = new Vector2(0, 0);
+    bool foundPlayerPosition = false;
+
+    public float dialogDistance;
+
+    public string interruptDialog;
+    private MiniBubbleController bubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (alreadyTalking)
+        //    talkingToPlayer();
+        //else if (!finishTalkingSequence)
+        //    idle();
+
+        if (alreadyTalking)
+        {
+            if (Vector2.Distance(interactor.transform.position, transform.position) >= dialogDistance)
+            {
+                exitDialog();
+                GameObject addedObj = Instantiate(miniBubblePrefab, transform.position + (Vector3)miniBubbleOffset, Quaternion.identity);
+                bubble = addedObj.GetComponent<MiniBubbleController>();
+                bubble.speaker = this;
+                bubble.offset = miniBubbleOffset;
+                bubble.setSource(new DialogSource(interruptDialog));
+            }
+        }
+    }
+
+    private void idle()
+    {
+
+    }
+
+    public override void interact(Entity user)
+    {
+        base.interact(user);
+        if (bubble != null && bubble.gameObject != null)
+            bubble.close();
+    }
+}
