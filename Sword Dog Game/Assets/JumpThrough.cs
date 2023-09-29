f50a5af46303bc3b6f5c607f1844724c4e888168
RJWMD <113476572+RJWMD@users.noreply.github.com>
2023-09-29 16:56:02 -0400

    some collider fixes in cave 3, you can now go down through any platforms by pressing down

--- Sword Dog Game/Assets/JumpThrough.cs
+++ Sword Dog Game/Assets/JumpThrough.cs
@@ -7,11 +7,14 @@ public class JumpThrough : MonoBehaviour
     private PlayerMovement player;
     private float height;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    public Collider2D cldr;
+
 
     // Start is called before the first frame update
     void Start()
     {
         height = spriteRenderer.bounds.size.y / 2;
+        cldr = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -25,7 +28,16 @@ public class JumpThrough : MonoBehaviour
         {
             // Debug.DrawLine((Vector3)player.bottom, transform.position - new Vector3(0, height));
             bool playerBelow = player.bottom.y < transform.position.y - height;
-            gameObject.layer = LayerMask.NameToLayer(playerBelow ? "Utility": "Terrain");
+            
+            //gameObject.layer = LayerMask.NameToLayer(playerBelow || Input.GetAxisRaw("Vertical") < -.5f ? "Utility": "Terrain");
+            if(playerBelow || Input.GetAxisRaw("Vertical") < -.5f)
+            {
+                Physics2D.IgnoreCollision(cldr, PlayerMovement.controller.cldr, true);
+            }
+            else
+            {
+                Physics2D.IgnoreCollision(cldr, PlayerMovement.controller.cldr, false);
+            }
         }
     }
 }
