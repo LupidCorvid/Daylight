40709f68fd1caaafc4cc37c7d00b45c835aa07c3
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-02-16 16:44:56 -0500

    Fixed stamina bar having having 20% more stamina than the bar showed

--- Sword Dog Game/Assets/StaminaBar.cs
+++ Sword Dog Game/Assets/StaminaBar.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class StaminaBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
+    public Image fillImage;
 
     void Update()
     {
-        if(Player.controller != null)
-            slider.value = Player.controller.stamina / Player.controller.maxStamina;
+        //if(Player.controller != null)
+        //    slider.value = Player.controller.stamina / Player.controller.maxStamina;
+        if (Player.controller != null)
+            fillImage.fillAmount = Player.controller.stamina / Player.controller.maxStamina;
     }
 }
