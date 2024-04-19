dba21ac51cbbb05b215c72fc0f6a62101cf892c4
EvanMcRae <evanmmcrae@gmail.com>
2024-04-19 16:47:06 -0400

    Bugfixes and improvements
    - Stamina bar pulses only when being consumed
    - Loose items render in front of trees
    - Fixed player going invis in Forest 3/4 transitions
    - Improved parallax bush rendering in Forest 3

--- Sword Dog Game/Assets/StaminaBar.cs
+++ Sword Dog Game/Assets/StaminaBar.cs
@@ -7,6 +7,8 @@ public class StaminaBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     public Image fillImage;
+    public Animator animator;
+    private bool pulsing;
 
     void Update()
     {
@@ -14,9 +16,22 @@ public class StaminaBar : MonoBehaviour
         //    slider.value = Player.controller.stamina / Player.controller.maxStamina;
         
         fillImage ??= GetComponent<Image>();
+        animator ??= GetComponent<Animator>();
 
         if (Player.controller != null)
+        {
             fillImage.fillAmount = Player.controller.stamina / Player.controller.maxStamina;
+            if (Player.controller.isSprinting && !pulsing)
+            {
+                animator.SetTrigger("start");
+                pulsing = true;
+            }
+            if (!Player.controller.isSprinting && pulsing)
+            {
+                animator.SetTrigger("stop");
+                pulsing = false;
+            }
+        }
 
         
     }
