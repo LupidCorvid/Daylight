64758e98f73de6d68ae506a118f3cfb8f43b3545
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-05-30 21:43:57 -0400

    Added an ocean test section to test scene

--- Sword Dog Game/Assets/WaterFX.cs
+++ Sword Dog Game/Assets/WaterFX.cs
@@ -43,6 +43,10 @@ public class WaterFX : MonoBehaviour
     public bool changeOnZoom = true;
     public bool onlyChangeOnGreaterZoom = true;
 
+    public float distantWavesScalar = 1;
+    public float distantWavesVolatilityScalar = 1;
+    public float distantWavesFalloffDistance = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -284,7 +288,7 @@ public class WaterFX : MonoBehaviour
             
             Vector3 worldSpace = transform.position + new Vector3(((i - (numSegments * 2 + 2))/2 * vertexDistance - size.x / 2), size.y/2);
             float depth = (i - (numSegments * 2 + 2)) % 2 == 0 ? zWidth : zStartDist;
-            worldSpace.y += SwayEffect.getWindEffect(worldSpace.x, windSpeed/7, windVolatility, windStrength, true) * 10/(Mathf.Clamp(Mathf.Abs(cam.transform.position.y - worldSpace.y) * .5f, 1, 20));
+            worldSpace.y += SwayEffect.getWindEffect(worldSpace.x, windSpeed/7, windVolatility * distantWavesVolatilityScalar, windStrength, true) * 10 * distantWavesScalar/(Mathf.Clamp(Mathf.Abs(cam.transform.position.y - worldSpace.y) * .5f * distantWavesFalloffDistance, 1, 20 * distantWavesScalar));
 
             if (changeOnZoom && (!onlyChangeOnGreaterZoom || cam.orthographicSize > 5))
                 newVerts[i] = new Vector3(worldSpace.x + ((worldSpace.x - cam.transform.position.x) * depth * 5f / cam.orthographicSize), worldSpace.y + ((worldSpace.y - cam.transform.position.y) * -depth / 5f * 5f / cam.orthographicSize), transform.position.z) - transform.position;
