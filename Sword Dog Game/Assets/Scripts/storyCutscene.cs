ca02c6dc31cba2a760af657bed98a2a3d8b3ae26
LupidCorvid <lucidcanine921@gmail.com>
2024-12-06 16:05:52 -0500

    Update storyCutscene.cs

--- Sword Dog Game/Assets/Scripts/storyCutscene.cs
+++ Sword Dog Game/Assets/Scripts/storyCutscene.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class storyCutscene
+public class storyCutscene : MonoBehaviour
 {
     enum CutsceneTypes {DIALOGUE, IN_GAME, ANIMATED}; //Dialogue: Required dialogue that the player cannot skip or walk away from
                                                       //In-game: Animated using in-game assets. Player cannot move
@@ -12,6 +13,20 @@ public class storyCutscene
     bool hasTriggered = false; //If the cutscene has been instantiated yet
     bool hasFinished = false; //If the cutscene has finished. Player cannot go back to it.
     bool isPlaying = false; //If the cutscene is currently playing
+
+    /*Some sort of cutscene contents variable?
+     * Dialogue just has the letter + number
+     * In-game would have some sort of animation and camera movement. Code is required. So this file might get long 
+     *      (or it can trigger subclasses. Inheretance moment?)
+     * Animated will have their own Scenes, so swap scenes
+    */
 }
-/* This class is an object for story relevant dialogue and cutscenes
+/* This class is an object for story relevant dialogue and cutscenes.
+ * Empty gameobjects for each cutscene will be in the scene, and when relevant criteria is made, the cutscene will trigger
+ * Criteria to Trigger a cutscene:
+ *      Talk to an NPC
+ *      Walk in the vicinity of an area/NPC
+ *      Kill an enemy/boss
+ *      Inspect something of importance
+ *      Finish/start a chapter
 */
