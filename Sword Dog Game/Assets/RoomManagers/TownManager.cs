c511edd9df512c86b0747fb1b594ff06c11e1009
LupidCorvid <lucidcanine921@gmail.com>
2025-02-28 13:58:27 -0500

    Created mangers and room states

---
+++ Sword Dog Game/Assets/RoomManagers/TownManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownManager : RoomManager
+{
+}
+
+public class DojoManager : RoomManager
+{
+}
+
+public class MedbayManager : RoomManager
+{
+}
